dc954001192b3667cbd3e56bb6d0a57db1e66279
sparuchuri <sparuchuri@lifetouch.com>
2019-07-20 09:10:23 -0500

    intisl commit

---
+++ src/main/groovy/com/swagg/swaggeruipoc/SwaggeruipocApplication.groovy
@@ -0,0 +1,13 @@
+package com.swagg.swaggeruipoc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SwaggeruipocApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SwaggeruipocApplication, args)
+	}
+
+}
